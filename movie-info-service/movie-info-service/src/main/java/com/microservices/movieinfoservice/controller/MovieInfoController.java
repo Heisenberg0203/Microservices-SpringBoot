65f7276c739f783c630df3327fb41be040027d83
Rushikesh <rushikesh0203@gmail.com>
2021-10-17 20:18:54 +0530

    Dummy Movie Info Service on port 8081

---
+++ movie-info-service/movie-info-service/src/main/java/com/microservices/movieinfoservice/controller/MovieInfoController.java
@@ -0,0 +1,15 @@
+package com.microservices.movieinfoservice.controller;
+
+import com.microservices.movieinfoservice.beans.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieInfoController {
+
+    @GetMapping("movies/{movieId}")
+    public Movie getMovie(@PathVariable("movieId") String movieId){
+        return new Movie(movieId,"TestMovieName");
+    }
+}
