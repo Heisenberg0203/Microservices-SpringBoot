49874150ecc588bd4f3c7d81122623163aedca93
Rushikesh <rushikesh0203@gmail.com>
2021-12-28 17:43:16 +0530

    Service 1: Added models for Movie,Rating

---
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/bean/Movie.java
@@ -0,0 +1,49 @@
+package com.microservices.moviecatalogservice.bean;
+
+public class Movie {
+    private String movieId;
+    private String movieName;
+    private String description;
+
+    public Movie() {
+    }
+
+    public Movie(String movieId, String movieName, String description) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.description = description;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieId='" + movieId + '\'' +
+                ", movieName='" + movieName + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
