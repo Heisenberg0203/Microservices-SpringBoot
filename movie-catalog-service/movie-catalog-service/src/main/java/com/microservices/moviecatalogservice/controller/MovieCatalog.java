857e879b1c489958da5053dea552d054b6ce698a
Rushikesh <rushikesh0203@gmail.com>
2021-12-28 17:44:21 +0530

    Service 1: Initiated communication between MovieCatalogController(S1) and MovieInfoController(S2)

--- movie-catalog-service/movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/controller/MovieCatalog.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/controller/MovieCatalog.java
@@ -1,10 +1,15 @@
 package com.microservices.moviecatalogservice.controller;
 
 import com.microservices.moviecatalogservice.bean.CatalogItem;
+import com.microservices.moviecatalogservice.bean.Movie;
+import com.microservices.moviecatalogservice.bean.Rating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,8 +19,27 @@ public class MovieCatalog {
 
     @GetMapping("catalog/{userId}")
     public List<CatalogItem> getCatalogs(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Transformer","Test",4.0f)
+
+        // step1: get all rated movieIds,
+        List<Rating> ratings = Arrays.asList(
+                new Rating("111",4),
+                new Rating("122",3)
         );
+
+        // step2: for each movieId get description from movie-info service
+        RestTemplate  restTemplate = new RestTemplate();
+        List<CatalogItem> catalogItems = new ArrayList<>();
+
+        for(Rating rating:ratings){
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            catalogItems.add(new CatalogItem(movie.getMovieName(),movie.getDescription(),rating.getRating()));
+        }
+
+        return  catalogItems;
+
+//        dummy api
+//        return Collections.singletonList(
+//                new CatalogItem("Transformer","Test",4.0f)
+//        );
     }
 }
